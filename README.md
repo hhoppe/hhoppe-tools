ce7e2038b7c52f01aba19ab91827454d72316745
Hugues Hoppe <hhoppe@gmail.com>
2020-12-12 10:56:45 -0800

    minor

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Example Package
+# hhoppe_utils
 
-This is a simple example package. You can use
-[Github-flavored Markdown](https://guides.github.com/features/mastering-markdown/)
-to write your content.
+Library of Python tools including some for `Jupyter`/`Colab`.
+
+[]: # (See https://guides.github.com/features/mastering-markdown/)
--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+with open('README.md') as f:
+  long_description = f.read()
 
 setuptools.setup(
-    name="hhoppe_utils",
-    version="0.0.1",
-    author="Hugues Hoppe",
-    author_email="hhoppe@gmail.com",
-    description="A small example package",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/hhoppe/hhoppe_utils",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
+  name='hhoppe_utils',
+  version='0.0.1',
+  author='Hugues Hoppe',
+  author_email='hhoppe@gmail.com',
+  description='Library of Python tools including some for Jupyter/Colab',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  url='https://github.com/hhoppe/hhoppe_utils.git',
+  packages=['hhoppe_utils'],  # packages=setuptools.find_packages(),
+  classifiers=[
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+  ],
+  python_requires='>=3.6',
 )
