5ae37dc4b6e82adbd647c1a0e825e9c8a6cf8599
Hugues Hoppe <hhoppe@users.noreply.github.com>
2021-12-12 22:49:27 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@ import setuptools
 
 with open('README.md') as f:
   long_description = f.read()
+with open('requirements.txt') as f:
+  requirements = [line.strip() for line in f]
 
 setuptools.setup(
   name='hhoppe_utils',
   version='0.0.1',
   author='Hugues Hoppe',
   author_email='hhoppe@gmail.com',
-  description='Library of Python tools including some for Jupyter/Colab',
+  description='Library of Python tools by Hugues Hoppe',
   long_description=long_description,
   long_description_content_type='text/markdown',
   url='https://github.com/hhoppe/hhoppe_utils.git',
@@ -18,5 +20,6 @@ setuptools.setup(
     'License :: OSI Approved :: MIT License',
     'Operating System :: OS Independent',
   ],
-  python_requires='>=3.6',
+  python_requires='>=3.7',
+  install_requires=requirements,
 )
