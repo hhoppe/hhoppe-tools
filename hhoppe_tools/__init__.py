d0e8987efeaf14dcfff49d110f3165db0636cb95
Hugues Hoppe <hhoppe@gmail.com>
2022-07-29 18:01:08 -0700

    Minor.

--- hhoppe_tools/__init__.py
+++ hhoppe_tools/__init__.py
@@ -20,7 +20,7 @@ gpylint hhoppe_tools.py
 """
 
 __docformat__ = 'google'
-__version__ = '0.9.0'
+__version__ = '0.9.1'
 __version_info__ = tuple(int(num) for num in __version__.split('.'))
 
 import ast
@@ -151,8 +151,8 @@ def check_eq(a: Any, b: Any) -> None:
   ...
   AssertionError: 3 == 4
   """
-  equal = np.all(a == b) if isinstance(a, np.ndarray) else a == b
-  if not equal:
+  are_equal = np.all(a == b) if isinstance(a, np.ndarray) else a == b
+  if not are_equal:
     raise AssertionError(f'{a!r} == {b!r}')
 
 
@@ -545,7 +545,7 @@ def prun(func: Callable[[], Any], mode: str = 'tottime',
   assert callable(func)
   assert mode in ('original', 'full', 'tottime'), mode
   site_packages = np.__path__[0][:-5]
-  assert site_packages.endswith('/site-packages/')
+  assert site_packages.endswith(('/site-packages/', '/dist-packages/'))
 
   profile = cProfile.Profile()
   try:
--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+"""Setup for pip package."""
+
 import pathlib
 import setuptools
 
@@ -14,13 +16,10 @@ def get_version(package=None):
   raise RuntimeError(f'Unable to find version string in {path}.')
 
 
-def get_long_description():
-  return pathlib.Path('README.md').read_text()
-
-
 def get_requirements():
-  with open('requirements.txt') as f:
-    return [line.strip() for line in f]
+  path = pathlib.Path('.') / 'requirements.txt'
+  return [line.strip() for line in path.read_text().splitlines()
+          if not (line.isspace() or line.startswith('#'))]
 
 
 setuptools.setup(
@@ -29,7 +28,7 @@ setuptools.setup(
   author='Hugues Hoppe',
   author_email='hhoppe@gmail.com',
   description='Library of Python tools by Hugues Hoppe',
-  long_description=get_long_description(),
+  long_description=pathlib.Path('README.md').read_text(),
   long_description_content_type='text/markdown',
   url=f'https://github.com/hhoppe/{NAME}.git',
   packages=setuptools.find_packages(),
@@ -37,9 +36,10 @@ setuptools.setup(
     package: ['py.typed'] for package in setuptools.find_packages()
   },
   classifiers=[
-    'Programming Language :: Python :: 3',
     'License :: OSI Approved :: MIT License',
     'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3',
+    'Topic :: Software Development :: Libraries :: Python Modules',
   ],
   python_requires='>=3.7',
   install_requires=get_requirements(),
